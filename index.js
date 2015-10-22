8c2e9ca4d448b059833588f606ac039d76daf566
hikaruna <hikaru.goldluna@gmail.com>
2015-10-22 10:52:26 +0900

    fix 終了しない問題

--- index.js
+++ index.js
@@ -26,32 +26,29 @@ app.on('ready', function() {
 
   // ウィンドウが閉じられたらアプリも終了
   mainWindow.on('closed', function() {
-    mainWindow = null;
+    app.quit();
   });
 
-var Menu = require("menu");
+  var Menu = require("menu");
 
-var template = [{
-    label: "Application",
-    submenu: [
-        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-        { type: "separator" },
-        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-    ]}, {
-    label: "Edit",
-    submenu: [
-        { label: "Undo", accelerator: "Command+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "Command+X", selector: "cut:" },
-        { label: "Copy", accelerator: "Command+C", selector: "copy:" },
-        { label: "Paste", accelerator: "Command+V", selector: "paste:" },
-        { label: "Select All", accelerator: "Command+A", selector: "selectAll:" },
-        { label: "Open google", accelerator: "Command+Q", click: function() {
-          require("shell").openExternal("http://www.google.com")
-        }}
-    ]}
-];
+  var template = [{
+      label: "Application",
+      submenu: [
+          { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+          { type: "separator" },
+          { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]}, {
+      label: "Edit",
+      submenu: [
+          { label: "Undo", accelerator: "Command+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "Command+X", selector: "cut:" },
+          { label: "Copy", accelerator: "Command+C", selector: "copy:" },
+          { label: "Paste", accelerator: "Command+V", selector: "paste:" },
+          { label: "Select All", accelerator: "Command+A", selector: "selectAll:" }
+      ]}
+  ];
 
-Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
